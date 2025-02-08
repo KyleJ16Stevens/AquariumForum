b624bdd3b8fe786f94e6a45f3e5a733af63a6082
Kyle Stevens <kj16stevens@gmail.com>
2025-02-08 17:03:45 -0400

    added comment and discummsion

---
+++ Models/Comment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AquariumForum.Models
+{
+    public class Comment
+    {
+        [Key] //primary Key
+        public int CommentId { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTime CreateDate { get; set; } = DateTime.Now; //auto-set date
+
+        //foreign Key to Discussion
+        [Required]
+        public int DiscussionId { get; set; }
+
+        //navigation property many comments belong to one discussion
+        [ForeignKey("DiscussionId")]
+        public Discussion Discussion { get; set; }
+    }
+}
---
+++ Models/Discussion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace AquariumForum.Models
+{
+    public class Discussion
+    {
+        [Key] //primary Key
+        public int DiscussionId { get; set; }
+
+        [Required]
+        [StringLength(200)] //limit title length
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        public string? ImageFilename { get; set; } //nullable if no image
+
+        [Required]
+        public DateTime CreateDate { get; set; } = DateTime.Now; //autoset date
+
+        //navigation property one discussion has many comments
+        public ICollection<Comment> Comments { get; set; } = new List<Comment>();
+    }
+}
